21bc64ff4614994dc10e67e3a9589c552524e330
Johnson Dubula <jdubula@iqbusiness.net>
2020-06-10 02:30:59 +0200

    html set up done

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+let btn = document.getElementById("btn");
+let color = document.querySelector(".color");
+
+btn.addEventListener('click', function() {
+    let randomNumber = 2;
+    document.body.style.borderBlockEndColor = colors[randomNumber];
+
+});
